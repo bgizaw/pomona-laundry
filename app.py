6c9dc169aed3416502a3256341ec6f4ae085ab9d
tesfaasmara <57428343+TesfaAsmara@users.noreply.github.com>
2024-05-29 14:47:55 -0500

    added gitignore, removed env, added comment for timer, updated README

--- app.py
+++ app.py
@@ -5,47 +5,42 @@ app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 class Washer:
-    def __init__ (self, state, endTime, ):
+    def __init__(self, state):
         self.state = state
-        
-        startTime = time.time()
-        endTime = startTime + 1740
-        
+        self.start_time = time.time()
+        self.end_time = self.start_time + 1740  # 1740 seconds = 29 minutes
 
 class Dryer:
-    def __init__ (self, state, timer):
+    def __init__(self, state, timer=0):
         self.state = state
         self.timer = timer
 
-    def add_time ():
-        self.timer 
+    def add_time(self, additional_time):
+        self.timer += additional_time
 
-    
 class Building:
-    def __init__(self, Washer, Dryer, Chat_Memory, WasherNum, DryerNum):
+    def __init__(self, washer_num, dryer_num):
         self.machines = []
-        self.chat_Memory = []
-        self.washer_num = 0
-        self.dryer_num = 0
+        self.chat_memory = []
+        self.washer_num = washer_num
+        self.dryer_num = dryer_num
 
-    def machine_add (machine):
+    def add_machine(self, machine):
         self.machines.append(machine)
-        
 
-    
-
-@app.route("/") #home page
+@app.route("/")  # Home page
 def index():
-    buildings = ['Sontag', 'Dialynas', 'Norton', 'Lawry', 'Clark 1', 'Clark 3', 'Clark 5', 'Walker', 'Smiley', 'Oldenborg', 'Blaisdell', 'Mudd', 'Lyon', 'Harwood', 'Gibson', 'Wig']
+    buildings = [
+        'Sontag', 'Dialynas', 'Norton', 'Lawry', 'Clark 1', 'Clark 3',
+        'Clark 5', 'Walker', 'Smiley', 'Oldenborg', 'Blaisdell', 'Mudd',
+        'Lyon', 'Harwood', 'Gibson', 'Wig'
+    ]
     return render_template('index.html', buildings=buildings)
 
-
-@app.route('/<building>') #page of machines for given building
+@app.route('/<building>')  # Page of machines for given building
 def buildingFunc(building):
     machines = ['Washer1', 'Washer2', 'Washer3', 'Washer4', 'Dryer1', 'Dryer2', 'Dryer3', 'Dryer4']
     return render_template('building.html', machines=machines)
 
-
 if __name__ == "__main__":
     app.run(debug=True, host="127.0.0.1", port=5000) #debug=True allows for auto updates. without this you need to restart server to see updates
-
