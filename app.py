b2413df0c90343f33c8331d9eced2c1c21a3ad4e
Biruk Gizaw <45443263+bgizaw@users.noreply.github.com>
2024-06-10 11:09:29 -0500

    added clarifying comments to building and machine classes

--- app.py
+++ app.py
@@ -7,14 +7,23 @@ import os
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
+# create a washer class that will initialize with two parameters, to represent if a washer is available/working and 
+# the real world time the machine will finish its cycle based on a datetime object
 class Washer:
     def __init__(self, state, end_time):
+        # will represent states like available, in use, pending, out of order, reserved
         self.state = state
+        # what time the washer wil finish the cycle
         self.end_time = end_time
 
     def start_washer(self, setting):
+        # acces the user's current browser time to reference
         start_time = datetime.datetime.now()
+        # use timedelta to put the amount of time the user's machine will run for into a datetime object format
+        # this will allow us to add it to their current browser time since that is also in a datetime object format
         time_change = datetime.timedelta(minutes=setting)
+        # add the amount of time the machine will run for to the user's current time, resulting in the datetime object
+        # formatted time for when their machine cycle will finish
         self.end_time = start_time + time_change
 
     def change_state(self, state):
@@ -32,8 +41,13 @@ class Washer:
             #comment
 
     def cycle_complete(self, browserTime):
+        # return a boolean True or False. We already have the end time of the cycle set in the start washer function
+        # and the moment the user's browser time passes that set end time, meaning the time set for the cycle to finish
+        # has already passed, the boolean will switch from returning False to True for if the cycle is complete
         return self.end_time < browserTime
 
+    # format the Washer class object in the String format below. Without this, the class object will print as a class 
+    # object, making debugging more difficult
     def __str__(self) -> str:
         return "Washer:" + " " + self.state + " " + "-" + self.time_remaining
 
@@ -58,32 +72,47 @@ class Dryer:
 
 class Building:
     def __init__(self, name, washer_num, dryer_num):
+        # the name of the building that the respective laundry room is related to
         self.name = name
+        # supposed to represent the washers and dryers in each laundry room and whether they are available or not
+        # but is not needed anymore due to the presence of our json file with all laundry room info
         self.machines = {
             "Washers": [],
             "Dryers" : []
         }
+        # would have represented our chat memory but seems inefficient compared to better options offered by Node
         self.chat_memory = []
+        # the number of washers in a given laundry room
         self.washer_num = washer_num
+        # the number of dryers in a given laundry room
         self.dryer_num = dryer_num
 
+        # not needed because of the JSON file with all machine data. These for loops would have populated the 
+        # washer and dryer lists within the machines dictionary with the correct number of machines given the 
+        # correct washer and dryer nums
         for washer in range(washer_num):
             self.machines["Washers"].append(Washer("available", 0))
 
         for dryer in range(dryer_num):
             self.machines["Dryers"].append(Dryer("available", 0))
 
+    # adds a machine to the dictionary but does it incorrectly. we would have to key into the dictionary first
+    # then append to the list in the value
     def add_machine(self, machine):
         self.machines.append(machine)
 
     def __str__(self):
         return "Building: " + self.name + "  " + "washers: " + str(self.washer_num) + "  " + "dryers: " + str(self.dryer_num)
 
+# open and read the JSON file with all the laundry room data within it, then put that list of dictionaries into 
+# data, which will represent a list of dictionaries
 with open("templates/machineInfo.json", "r") as json_file:
     data = json.load(json_file)
 
-building_list = [Building(item['building'], item['washers'], item['dryers'])
+# list comprehension to 
+building_list = [Building(item['building'], item['washers'], item['dryers']) 
                  for item in data]
+print(building_list[0])
 
 
 # Home page
@@ -127,9 +156,11 @@ def buildingFunc(building):
 @app.route('/washer/<building>/<washer>/', methods=['GET'])
 def updateWasherFileData(building, washer):
     # creating a folder when someone goes to the washer
-    newpath = f"buildings/{building}/{washer}/"
+    newpath = f"buildings/{building}/{washer}/" #The file path of the folder that washer that the washer data file is within
     if request.method == 'GET':
+        #get method is attempting to retrieve information from the stored data
         if not os.path.exists(newpath):
+            #if that data does not currently exist in the database, create a new file for the 
             os.makedirs(newpath)
             file = open(f"{newpath}machine_info.txt", 'w')
             file.write("Status: Available\n")
@@ -141,6 +172,7 @@ def updateWasherFileData(building, washer):
             
             time_data = data[1].split('Completion Time:')
             time_list = time_data[1].strip().split('/')
+            # write error handling for this
             completion_time = datetime.datetime(int(time_list[0]),int(time_list[1]), int(time_list[2]), int(time_list[3]), int(time_list[4]), int(time_list[5]))
             time_remaining = completion_time - datetime.datetime.now() 
             
@@ -342,8 +374,8 @@ def updateDryerTimeFunc():
         return render_template('dryer_status.html', content = content, building=building, dryer=dryer)
 
 
-if __name__ == "__main__":
+# if __name__ == "__main__":
     # debug=True allows for auto updates. 
     # Without this you need to restart server to see updates.
     # Moreover, our port is currently on 8000.
-    app.run(debug=True, host="127.0.0.1", port=8000)
+    # app.run(debug=True, host="127.0.0.1", port=8000)
